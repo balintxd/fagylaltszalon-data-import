77d0c3e6491b73ddc03130a7aabef5ffd4338823
Balint <nagybalinthu@gmail.com>
2022-06-09 19:53:45 +0200

    Update code

--- import.py
+++ import.py
@@ -1,43 +1,63 @@
+import os
 from slugify import slugify
+from dotenv import load_dotenv
+from woocommerce import API
+
+load_dotenv()
+
+wcapi = API(
+    url=os.getenv("WC_URL"),
+    consumer_key=os.getenv("WC_CONSUMER_KEY"),
+    consumer_secret=os.getenv("WC_CONSUMER_SECRET"),
+    version='wc/v3'
+)
 
 class Termek:
-    def __init__(self, sorszam, megnevezes, szelet, mertekegyseg, egysegar, katekoria,
+    def __init__(self, sorszam, megnevezes, szelet, mertekegyseg, egysegar, kategoria,
                 valtozat, idenyjellegu, burkolhato, ostya, glutenmentes, laktozmentes,
                 paleo, termekleiras, megjegyzes, atveheto, atveheteo_alt):
-        self.sorszam = sorszam
-        self.megnevezes = megnevezes
-        self.szelet = szelet
-        self.mertekegyseg = mertekegyseg
-        self.egysegar = egysegar
-        self.katekoria = katekoria
-        self.valtozat = valtozat
-        self.idenyjellegu = idenyjellegu
-        self.burkolhato = burkolhato
-        self.ostya = ostya
-        self.glutenmentes = glutenmentes
-        self.laktozmentes = laktozmentes
-        self.paleo = paleo
-        self.termekleiras = termekleiras
-        self.megjegyzes = megjegyzes
-        self.atveheto = atveheto
-        self.atveheteo_alt = atveheteo_alt
 
-        self.rovid_leiras = self.termekleiras[0:40]
-        if len(self.rovid_leiras < self.termekleiras):
+        self.sorszam = int(sorszam)
+        self.megnevezes = megnevezes
+        self.szelet = int(szelet) if szelet != '' else None
+        self.mertekegyseg = mertekegyseg
+        self.egysegar = int(egysegar)
+        self.kategoria = kategoria
+        self.valtozat = valtozat.split(',') if valtozat != '' else None
+        self.idenyjellegu = True if idenyjellegu == 'Idényjellegű' else False
+        self.burkolhato = True if burkolhato == 'Igen' else False
+        self.ostya = True if ostya == 'Igen' else False
+        self.glutenmentes = True if glutenmentes == 'Igen' else False
+        self.laktozmentes = True if laktozmentes == 'Igen' else False
+        self.paleo = True if paleo == 'Paleo' else False
+        self.termekleiras = termekleiras.capitalize() if termekleiras != '' else None
+        self.megjegyzes = megjegyzes if megjegyzes != '' else None
+        self.atveheto = int(atveheto) if atveheto != '' else None
+        self.atveheteo_alt = atveheteo_alt if atveheteo_alt != '' else None
+        self.rovid_leiras = self.termekleiras[0:37] if self.termekleiras else None
+
+        if  self.rovid_leiras != None and len(self.rovid_leiras) < len(self.termekleiras):
             self.rovid_leiras += '...'
+
+    def __str__(self):
+        return '{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(self.sorszam, self.megnevezes, self.szelet, self.mertekegyseg, self.egysegar, self.kategoria,
+            self.valtozat, self.idenyjellegu, self.burkolhato, self.ostya, self.glutenmentes, self.laktozmentes, self.paleo, self.termekleiras, self.rovid_leiras, 
+            self.megjegyzes, self.atveheto, self.atveheteo_alt)
 
 lines = open('termekek.tsv', mode='r', encoding='utf-8').readlines()[1:]
 
 termekek = []
 
 for line in lines:
-    item = line.split('\t').strip()
+    item = line.split('\t')
     termek = Termek(item[0], item[1], item[2], item[3], item[4], item[5], item[6], 
                     item[7], item[8], item[9], item[10], item[11], item[12], 
-                    item[13], item[14], item[15])
+                    item[13], item[14], item[15], item[16])
     termekek.append(termek)
 
-for termek in termekek:
+print(termekek[26])
+
+"""for termek in termekek:
     product_data = {
         "name": termekek.megnevezes,
         "slug": slugify(termekek.megnevezes),
@@ -74,4 +94,4 @@ for termek in termekek:
     }
 
     product = wcapi.post('products', data).json()
-wcapi.post('products/{}/variations'.format(product['id']), variation_data).json()
+    wcapi.post('products/{}/variations'.format(product['id']), variation_data).json()"""
